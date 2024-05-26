f3c155ddeca58bb11b6ceadb5b3cc506d9e263c4
조현우 <qksksk785@gmail.com>
2024-05-26 11:15:36 +0000

    동아리 관리

---
+++ dongproject/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+	<nav class="navbar">
+		<ul>
+			<li><NuxtLink to="/">Home</NuxtLink></li>
+			<li><NuxtLink to="/about">About</NuxtLink></li>
+			<li><NuxtLink to="/login">Login</NuxtLink></li>
+		</ul>
+	</nav>
+</template>
+
+<style>
+	.navbar {
+		background-color: #333;
+		padding: 1rem;
+	}
+	.navbar ul {
+		list-style-type: none;
+		display: flex;
+		gap: 1rem;
+	}
+	.navbar a {
+		color: white;
+		text-decoration: none;
+	}
+</style>
