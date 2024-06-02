2eb1481b7dc014a8e218744a4f1a266c8a7a915b
조현우 <qksksk785@gmail.com>
2024-06-02 11:20:05 +0000

    before axios

---
+++ dongproject/dongproject/pages/login.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="container">
+		<div class="login-box">
+			<p class="title">Login</p>
+			<label>email</label>
+			<input v-model="email" type="email" id="email" required />
+			<label>password</label>
+			<input v-model="password" type="password" id="password" required />
+			<button type="submit" class="loginBtn">Login</button>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+	.container {
+		background-color: lightgray;
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+	}
+	.login-box {
+		background-color: lightsalmon;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		position: absolute;
+		top: 20vh;
+		width: 40vw;
+		height: 60vh;
+		border-radius: 1rem;
+	}
+	.title {
+		font-size: 3rem;
+		font-weight: 700;
+		margin-top: 1rem;
+		margin-bottom: 1rem;
+	}
+	.loginBtn {
+		margin-top: 1.5rem;
+		margin-bottom: 1rem;
+	}
+	@media (max-width: 450px) {
+		.login-box {
+			margin: 0 6rem;
+		}
+	}
+</style>
