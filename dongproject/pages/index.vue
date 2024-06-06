69b29fbe06cbafcb93932799b99cfa61592d087d
조현우 <qksksk785@gmail.com>
2024-06-06 09:49:40 +0000

    UI updated

--- dongproject/pages/index.vue
+++ dongproject/pages/index.vue
@@ -1,56 +1,48 @@
 <template>
 	<div class="container">
-		<div class="titleContainer">
-			<p class="title">DONG</p>
+		<div class="iconContainer">
+			<NuxtLink to="/login" class="dongIcon"></NuxtLink>
+			<p style="margin: 0; padding: 0; text-align: center;">PM</p>
 		</div>
-		<div class="loginBtnContainer">
-			<NuxtLink to="/login" class="loginLink">Login</NuxtLink>
+		<div class="iconContainer">
+			<NuxtLink to="/login" class="dongIcon" style="background-color: teal"></NuxtLink>
+			<p style="margin: 0; padding: 0; text-align: center;">Lab</p>
 		</div>
+		
+		<NuxtLink to="/login" class="createDong">NEW</NuxtLink>
 	</div>
 </template>
 
 <style scoped>
+	@media (max-width: 450px) {
+		.dongIcon {
+			display: block;
+			background-color: tomato;
+			width: 5rem;
+			height: 5rem;
+			border-radius: 1rem;
+			margin: 0rem 1rem;
+		}
+		.createDong {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			background-color: blue;
+			width: 4rem;
+			height: 4rem;
+			border-radius: 3rem;
+			margin: 0.5rem 1rem;
+			text-decoration: none;
+			color: white;
+			font-size: 1.5rem;
+			font-weight: 600;
+		}
+	}
 	.container {
 		background-color: lightgray;
 		height: 100vh;
-	}
-	.titleContainer {
-		height: 60vh;
-		/*background-color: teal;*/
+		padding: 2rem 1rem;
 		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-	.title {
-		font-size: 5rem;
-	}
-	.loginBtnContainer {
-		display: flex;
-		justify-content: center;
-	}
-	.loginLink {
-		font-size: 3.5rem;
-		text-decoration: none;
-		color: black;
-	}
-	.loginLink:hover {
-		color: salmon;
-	}
-	@media (max-width: 450px) {
-		.container {
-		background-color: lightgray;
-		display: flex;
-		justify-content: center;
-		flex-direction: column;
-		}
-		.title {
-			font-size: 50px;
-			text-align: center;
-		}
-		.loginLink {
-			font-size: 30px;
-			text-align: center;
-			text-decoration: none;
-		}
+		flex-direction: row;
 	}
 </style>
--- dongproject/pages/login.vue
+++ dongproject/pages/login.vue
@@ -1,31 +1,28 @@
 <template>
 	<div class="container">
 		<div class="login-box">
-			<p class="title">Login</p>
-			<label>email</label>
-			<input v-model="email" type="email" id="email" required />
-			<label>password</label>
-			<input v-model="password" type="password" id="password" required />
+			<p class="title">로그인</p>
+			<input v-model="username" type="text" id="username" class="inputs" placeholder="아이디" required />
+			<input v-model="password" type="password" id="password" class="inputs" placeholder="비밀번호" required />
 			<button type="submit" class="loginBtn">Login</button>
 		</div>
 	</div>
 </template>
 
+<script setup>
+	
+</script>
+
 <style scoped>
 	.container {
 		background-color: lightgray;
-		height: 100vh;
 		display: flex;
 		justify-content: center;
 	}
 	.login-box {
 		background-color: lightsalmon;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		flex-direction: column;
 		position: absolute;
-		top: 20vh;
+		top: 10vh;
 		width: 40vw;
 		height: 60vh;
 		border-radius: 1rem;
@@ -34,15 +31,32 @@
 		font-size: 3rem;
 		font-weight: 700;
 		margin-top: 1rem;
-		margin-bottom: 1rem;
+		margin-bottom: 3rem;
+		text-align: center;
 	}
 	.loginBtn {
-		margin-top: 1.5rem;
-		margin-bottom: 1rem;
+		position: absolute;
+		top: 45vh;
+		left: 50%;
+		transform: translateX(-50%);
+	}
+	.inputs {
+		display: block;
+		margin: 1rem auto;
 	}
 	@media (max-width: 450px) {
 		.login-box {
-			margin: 0 6rem;
+			width: 70vw;
+		}
+		.title {
+			font-size: 2rem;
+			margin-top: 3rem;
+		}
+		.inputs {
+			font-size: 1rem;
+		}
+		.loginBtn {
+			font-size: 1.2rem;
 		}
 	}
 </style>
